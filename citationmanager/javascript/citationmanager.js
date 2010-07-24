935f1f3be656847cf15d2385a0009e1a639922a7
zerocool1989 <manoj0011989@gmail.com>
2010-07-24 21:34:35 +0530

    UI working

--- citationmanager/javascript/citationmanager.js
+++ citationmanager/javascript/citationmanager.js
@@ -27,13 +27,13 @@ sakai.citationmanager=function(tuid,showSettings){
 		//Configuration Variables//
 		//////////////////////////
 	var citation_info;//Object That holds all the citatons of a user
-	var count_private;//holds the private citations count 
-	var count_public;//holds the public citaitons count
+	var count_private0;//holds the private citations count 
+	var count_public=0;//holds the public citaitons count
 	var parseCitations=[];//parse the returned citaitons from loadJSON
 	var $citationManagerMainTemplate = "citationmanager_main_citations_template";
 	var pageCurrent=0;
-	var pageSize=1;
-	var citationsArray=["public","private"];
+	var pageSize=5;
+	var citationsArray=[];
 	var citaitons=[];//array that contains a users citaions this is basically a copy of paging array intended for deleting and adding comments
 	sakai.config.URL.CONNOTEA_AUTHENTICATE_PROXY = "/var/proxy/citationmanager/connotea.json";//connotea Authenticate proxy
 	sakai.config.URL.CONNOTEA_FETCH_PROXY="/var/proxy/citationmanager/connotea_import.json";//connotea fetch_bookmarks proxy
@@ -99,13 +99,11 @@ sakai.citationmanager=function(tuid,showSettings){
 	 * @param {Object} arraylength the number of objects in tha array
 	 */
 	 var renderPaging = function(arraylength) {
-	 	//alert("asd12");
         $(".jq_pager1").pager({
             pagenumber: pageCurrent + 1,
             pagecount: Math.ceil(arraylength / pageSize),
             buttonClickCallback: doPaging
         });
-		//alert("asd");
     };
 	/**
 	 * Render the citations of a  user
@@ -115,42 +113,24 @@ sakai.citationmanager=function(tuid,showSettings){
 		for (var b in parseCitations.all){
 			if(parseCitations.all.hasOwnProperty(b)){
 				parseCitationsArray.push(parseCitations.all[b]);
-				
+				}
 			}
-			
-		}
 		citationsArray=parseCitationsArray;
-		
 		var pagingArray = {
             all: parseCitationsArray.slice(pageCurrent * pageSize, (pageCurrent * pageSize)+ pageSize )
         };
-		//$("<div>asd</div>");
-		//alert("asd");
 		$("#public_citations_view").html($.TemplateRenderer("citationmanager_main_citations_template",pagingArray));
-		//alert("asd");
 		if (parseCitationsArray.length > pageSize) {
-			//alert("hello");
-		
-            $("#citation_pager").show();
+		    $("#citation_pager").show();
             renderPaging(parseCitationsArray.length);
         }
-		
-		//alert(parseCitationsArray[1].UR);
-		//for (var i=0;i<parseCitationsArray.length;i++){
-			//$("#public_citations_view").append("<div class=citation_view>UR "+parseCitationsArray[i].UR+"<br/>TY "+parseCitationsArray[i].TY+"<br/>TL "+parseCitationsArray[i].TL+"<br/>N1 "+ parseCitationsArray[i].N1+"<br/>AU "+parseCitationsArray[i].AU+"<br/>KW "+parseCitationsArray[i].KW+"<br/>ER</div>");
-		//}
 	}
 	
 	/**
 	 * Show or hide the other properties of a citions
 	 */
 	var showHideCitationManagerBookmarkInfo = function() {
-
-        // Re-initialization
-        //$citationmanager_main_bookmark_info_link = $(".citationmanager_main_bookmark_info_link");
-
-        // Kill previous live events to prevent adding dupes
-       //$citationmanager_main_bookmark_info_link.die();
+		$(".citationmanager_main_bookmark_info_link").die();
 
         $(".citationmanager_main_bookmark_info_link").live("click", function() {
 
@@ -163,22 +143,13 @@ sakai.citationmanager=function(tuid,showSettings){
             {
                 $(elementID).slideUp(150);
                 $(this).removeClass("citationmanager_showArrowLeft");
-				//alert("asd1");
-				asd=0;
             }
-
             // Else: show information and swap image
             else
             {
                 $(elementID).slideDown(150);
                 $(this).addClass("citationmanager_showArrowLeft");
-				asd=1;
             }
-			$(".user-icon").live("click",function(){
-				$(this).hide();
-			});
-				
-			
         });
     };
 	/**
@@ -187,7 +158,8 @@ sakai.citationmanager=function(tuid,showSettings){
 	function addCitations(){
 		$("#addnotetextarea").hide();
 		$("#add").toggle();
-		$("addpublic").live("click",function(){
+		$("#addpublic").live("click",function(){
+			//sakai.api.Server.removeJSON("/_user" +sakai.data.me.profile.path+"/public/citationdata",alert('asdasd'));
 			sakai.api.Server.loadJSON("/_user" +sakai.data.me.profile.path+"/public/citationdata",function(success,data){
 				
 		 	if (success) 
@@ -198,11 +170,11 @@ sakai.citationmanager=function(tuid,showSettings){
 					if (citation_info.hasOwnProperty(key)) 
 					{
 						count_public++;//findout the current number of citations.
-						//alert(count2);
+						
 					}
 				}
 			}
-			else if(!success)
+			else 
 			{
 				var data2 =  
 				[{
@@ -215,11 +187,7 @@ sakai.citationmanager=function(tuid,showSettings){
 		
 				}]
 				
-					sakai.api.Server.saveJSON("/_user" + sakai.data.me.profile.path + "/public/citationdata", data2, afterSaveJSON(success));
-					alert("asd");
-				
-				
-			}
+					sakai.api.Server.saveJSON("/_user" + sakai.data.me.profile.path + "/public/citationdata", data2, alert("data saved"));}
 			var data1 =  
 			{
 			"UR" :$("#url").val(),
@@ -230,56 +198,192 @@ sakai.citationmanager=function(tuid,showSettings){
 			"KW":$("input.tag").val()
 		
 			}
-			
-            //alert(count2);
-		citation_info[count2]=data1;//add to the end of the json array
+		citation_info[count_public]=data1;//add to the end of the json array
 		sakai.api.Server.saveJSON("/_user" + sakai.data.me.profile.path + "/public/citationdata", citation_info, alert("data uploaded"));
 		 });
+		});
+			$("#addprivate").live("click",function(){
+			sakai.api.Server.loadJSON("/_user" +sakai.data.me.profile.path+"/private/citationdata",function(success,data){
+				
+		 	if (success) 
+			{
+				citation_info = data;
+				for (var key in citation_info) 
+				{
+					if (citation_info.hasOwnProperty(key)) 
+					{
+						count_private++;//findout the current number of citations.
+					}
+				}
+			}
+			else 
+			{
+				var data2 =  
+				[{
+					"UR" :$("#url").val(),
+					"AU":$("#author").val(),
+					"TL":$("#title").val(),
+					"TY":$("#typeofref").val(),
+					"N1":$("#addnotetextarea").val(),
+					"KW":$("input.tag").val()
+		
+				}]
+				
+					sakai.api.Server.saveJSON("/_user" + sakai.data.me.profile.path + "/private/citationdata", data2, alert("data saved"));
+					}
+			var data1 =  
+			{
+			"UR" :$("#url").val(),
+			"AU":$("#author").val(),
+			"TL":$("#title").val(),
+			"TY":$("#typeofref").val(),
+			"N1":$("#addnotetextarea").val(),
+			"KW":$("input.tag").val()
+		
+			}
+		citation_info[count_private]=data1;//add to the end of the json array
+		sakai.api.Server.saveJSON("/_user" + sakai.data.me.profile.path + "/private/citationdata", citation_info, alert("data uploaded"));
+		 });
+		});
+	}
+	/**
+	 * Add comments to a citaiton.
+	 */
+	function addComments(){
+		$(".citationmanager_main_bookmark_info_link_comment").die();
+		$(".comments_button").die();
+		$(".citationmanager_main_bookmark_info_link_comment").live("click",function(){
+			$(".textarea_comment").toggle('slow');
+			$(".comments_button").live("click",function(){
+				var comment=$(this).prev().val();
+				alert(comment);
+				var citation=$(this).attr("id");
+				var comment_Citation=citationsArray.slice(citation,1);
+				
+			});
 		});
 	}
 	/**Fetch The public and private citations of the user.
 	 * This is the default view of the citaiton manager widget on loading
 	 * 
 	 */
-	function fetchCitations(){
-		sakai.api.Server.loadJSON("/_user"+sakai.data.me.profile.path+"/public/citationdata",function(success,data){
+	function fetchCitations(ref_type){
+		if (ref_type=="public"){
+			sakai.api.Server.loadJSON("/_user"+sakai.data.me.profile.path+"/public/citationdata",function(success,data){
 			if(success)
 			{
-				//alert("success");
 				parseCitations={all:data};
+				$("#citation_pager").hide();
 				renderCitations();
 			}
-			alert("asd");
-			showHideCitationManagerBookmarkInfo();
-			$(".user-icon").live("click",function(){
+			});
+		}
+		else if (ref_type=="private"){
+			sakai.api.Server.loadJSON("/_user"+sakai.data.me.profile.path+"/private/citationdata",function(success,data){
+			if(success)
+			{
+				parseCitations={all:data};
+				$("#citation_pager").hide();
+				renderCitations();
+			}
+			});
+		}
+		else if(ref_type.charAt(0)=="/"){
+			alert("hello");
+				sakai.api.Server.loadJSON("/_user"+ref_type+"/public/citationdata",function(success,data){
+			if(success)
+			{
+				parseCitations={all:data};
+				$("#citation_pager").hide();
+				renderCitations();
+			}
+			});
+		}
+		
+	}
+	/**
+	 * Deletes a user's citaiton 
+	 */
+	function deleteCitation(){
+		$(".user-icon").live("click",function(){
+			alert("hello user");
 				var delete_citaiton=$(this).attr("id");
+				alert(delete_citaiton);
+				//alert(citationsArray.length);
+				alert(citationsArray[0].UR);
 				citationsArray.splice(delete_citaiton,1);
-				//alert("hello");
+				//alert(citationsArray.length);
+				alert(citationsArray[0].UR);
 				//alert(citationsArray[0].UR);
 				sakai.api.Server.removeJSON("/_user/"+sakai.data.me.profile.path+"/public/citationdata",alert("data removed"));
-				sakai.api.Server.saveJSON("/_user/"+sakai.data.me.profile.path+"/public/citationdata",citationsArray,alert("data loaded"));
+				sakai.api.Server.saveJSON("/_user/"+sakai.data.me.profile.path+"/public/citationdata",citationsArray,alert("data saved"));
 			});
-			
-		
-		});
-		
-		
-		
 	}
-	function clickEvents(){
-		$("#add").hide();
+	/**
+	 * 
+	 */
+	
+	function getCitations(refType){
+		
+		pageCurrent=0;
+		if (refType=="public"){fetchCitations("public");
+			showHideCitationManagerBookmarkInfo();
+			deleteCitation();
+			addComments();
+		}
+		else if(refType=="private"){
+			fetchCitations("private");
+			showHideCitationManagerBookmarkInfo();
+			deleteCitation();
+			addComments();
+		}
+		else if(refType.charAt(0)=="/"){
+			fetchCitations(refType);
+			showHideCitationManagerBookmarkInfo();
+			deleteCitation();
+			addComments();
+		}
 	}
 	
-	var doInit = function(){
-		clickEvents();
-		
-		//alert("hello");
-		fetchCitations();
+	
+	function clickEvents(){
+		$("#add").hide();
+		$("#public").die();
+		$("#private").die();
+		$("#public").live("click",function(){
+			refType = "public";
+			//alert("hello")
+			getCitations(refType);
+			
+		});
+		$("#private").live("click",function(){
+			refType = "private";
+			getCitations(refType);
+		});
 		$("#add_citations").live("click",function(){
 			$("#citations_view").toggle();
 			addCitations();
 		});
-		
+		$("#fetchfrom").live("click",function(){
+			$("#fetchfromuser").fadeIn('slow');
+			$("#fetch").live("click",function(){
+				//alert("asd");
+				var userStr=$("#fetch_citations_username").val();
+				var username="/"+userStr.charAt(0)+"/"+userStr.substr(0,2)+"/"+userStr;
+				//alert("asd1");
+				//alert(username);
+				refType=username;
+				getCitations(refType);
+			});
+			
+		});
+	}
+	/**
+	 * 
+	 */
+	var doInit = function(){
+		clickEvents();
+		//alert("hello");
 		
 	};
 	doInit();
