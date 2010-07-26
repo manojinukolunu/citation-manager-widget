1c2e55c7b62c7b14034bb2b4d0d24cdbfde622a7
zerocool1989 <manoj0011989@gmail.com>
2010-07-26 19:52:39 +0530

    Export RIS added

---
+++ citationmanager/ExportRIS.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.sakaiproject.nakamura.exportRIS;
+
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.jackrabbit.api.security.principal.PrincipalIterator;
+import org.apache.jackrabbit.api.security.principal.PrincipalManager;
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.Group;
+import org.apache.jackrabbit.api.security.user.UserManager;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.api.wrappers.ValueMapDecorator;
+import org.apache.sling.commons.json.JSONException;
+import org.apache.sling.jcr.base.util.AccessControlUtil;
+import org.sakaiproject.nakamura.api.doc.BindingType;
+import org.sakaiproject.nakamura.api.doc.ServiceBinding;
+import org.sakaiproject.nakamura.api.doc.ServiceDocumentation;
+import org.sakaiproject.nakamura.api.doc.ServiceMethod;
+import org.sakaiproject.nakamura.api.doc.ServiceResponse;
+import org.sakaiproject.nakamura.api.personal.PersonalUtils;
+import org.sakaiproject.nakamura.api.user.UserConstants;
+import org.sakaiproject.nakamura.util.ExtendedJSONWriter;
+import org.sakaiproject.nakamura.util.PathUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import java.io.*;
+import java.io.OutputStream;
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.TimeZone;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.Value;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+
+@SlingServlet(methods={"GET"}, paths={"/~user/public/citationsdata"}, extensions={"json"})
+@Properties(value={
+    @Property(name="service.description", value="Export RIS"),
+    @Property(name="service.vendor", value="The Sakai Foundation")
+})
+public class ExportRIS extends HttpServlet {
+  /**
+   *
+   */
+  private static final long serialVersionUID = -2002186252317448037L;
+
+ 
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+	  String filename="/_user/a/ad/admin/public/citationdata";
+	  File f=new File(filename);
+	  int length=0;
+	  ServletOutputStream op = resp.getOutputStream();
+	  ServletContext context  = getServletConfig().getServletContext();
+	  
+	  String mimetype = context.getMimeType( filename );
+	  resp.setContentType( (mimetype != null) ? mimetype : "application/octet-stream" );
+	  resp.setHeader( "Content-Disposition", "attachment; filename=Citations.RIS" );
+       byte[] bbuf = new byte[30];
+	   DataInputStream in = new DataInputStream(new FileInputStream(f));
+
+        while ((in != null) && ((length = in.read(bbuf)) != -1))
+        {
+            op.write(bbuf,0,length);
+        }
+
+        in.close();
+        op.flush();
+        op.close();
+    }
+
+}
